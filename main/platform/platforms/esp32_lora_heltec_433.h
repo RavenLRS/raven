51b5511083d085d41cb4df29b2a40ac8371da70a
Andre Bernet <kilrah@andrebernet.ch>
2018-05-26 14:01:48 +0200

    Add support for Heltec 433 module (only LED pin different)

---
+++ main/platform/platforms/esp32_lora_heltec_433.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define LORA_OUTPUT_TYPE LORA_OUTPUT_PA_BOOST
+#define USE_LORA_BAND_433
+#define CONFIG_LORA_BAND_DEFAULT CONFIG_LORA_BAND_433
+
+#define PIN_LED_1 25
+
+#include "platform/platforms/esp32_lora_heltec.h"
