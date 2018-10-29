1eb06ef32a76159f597e27f07d56f2e8cf1df2ae
hdphilip <hdphilip@yahoo.com>
2018-10-28 21:21:58 -0700

    change microRX V1 to MicroRX_RFM
    changed the target microrx v1 to udated target MicroRX_RFM to reflect a final revison pcb

--- main/target/platforms/esp32_lora_MicroRX_RFM_433/platform.h
+++ main/target/platforms/esp32_lora_MicroRX_RFM_433/platform.h
@@ -9,10 +9,11 @@
 
 #define BUTTON_1_GPIO 0
 
-#define BEEPER_GPIO 25 // This is Led D2 (red) on the microRX
+#define BEEPER_GPIO 23 // This is Led D2 (red) on the microRX
 #define LED_1_GPIO 4   // This is Led D1 (Green)
 
 #define USE_SCREEN
+#define SCREEN_I2C_BUS I2C_NUM_1
 #define SCREEN_GPIO_SDA 21
 #define SCREEN_GPIO_SCL 22
 // We don't use the OLED RST pin because it causes reboots on some boards
@@ -20,18 +21,21 @@
 #define SCREEN_GPIO_RST 0
 #define SCREEN_I2C_ADDR 0x3c
 
+#define SX127X_SPI_BUS VSPI_HOST
 #define SX127X_GPIO_SCK 5
 #define SX127X_GPIO_MISO 19
 #define SX127X_GPIO_MOSI 27
 #define SX127X_GPIO_CS 18
 #define SX127X_GPIO_RST 14
 #define SX127X_GPIO_DIO0 26
-#define SX127X_OUTPUT_TYPE SX127X_OUTPUT_PA_BOOST
-#define TX_DEFAULT_GPIO 23
-#define RX_DEFAULT_GPIO 23
 
-#define TX_UNUSED_GPIO 2
-#define RX_UNUSED_GPIO 35
+#define SX127X_OUTPUT_TYPE SX127X_OUTPUT_PA_BOOST
+
+#define TX_DEFAULT_GPIO 25
+#define RX_DEFAULT_GPIO 13
+
+#define TX_UNUSED_GPIO 13
+#define RX_UNUSED_GPIO 25
 
 #define HAL_GPIO_USER_MASK (HAL_GPIO_M(TX_DEFAULT_GPIO) | HAL_GPIO_M(RX_DEFAULT_GPIO))
-#define BOARD_NAME "ESP32+LoRa MicroRX v1"
+#define BOARD_NAME "ESP32+LoRa MicroRX_RFM"
