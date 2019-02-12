de08eb98dab42b71633a9a9748c5773bc908872c
Alberto García Hierro <alberto@garciahierro.com>
2019-02-12 20:59:42 +0000

    Reduce configMINIMAL_STACK_SIZE on STM32
    Make it 64. Also, make the stack for the RMP task 128 words, since
    64 causes stack overflows.

--- main/target/platforms/stm32/FreeRTOSConfig.h
+++ main/target/platforms/stm32/FreeRTOSConfig.h
@@ -46,7 +46,7 @@
 #define configCPU_CLOCK_HZ ((unsigned long)72000000)
 #define configTICK_RATE_HZ ((TickType_t)1000)
 #define configMAX_PRIORITIES (5)
-#define configMINIMAL_STACK_SIZE ((unsigned short)128)
+#define configMINIMAL_STACK_SIZE ((unsigned short)64)
 #define configTOTAL_HEAP_SIZE ((size_t)(4 * 1024))
 #define configMAX_TASK_NAME_LEN (16)
 #define configUSE_TRACE_FACILITY 0
--- main/target/platforms/stm32/f1/pre_platform.h
+++ main/target/platforms/stm32/f1/pre_platform.h
@@ -2,5 +2,5 @@
 #include <libopencm3/stm32/spi.h>
 
 #define RC_TASK_STACK_SIZE 512
-#define RMP_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define RMP_TASK_STACK_SIZE 128
 #define UI_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
