548848ba4c106499a9fe850e7add22ba1299f6c6
Alberto García Hierro <alberto@garciahierro.com>
2019-02-15 23:37:14 +0000

    Implement storage HAL for STM32
    Use wldb for the heavy lifting, implementing just a thin wrapper
    over it. For now, we're reserving 8k from flash using 2k blocks
    to perform wear leveling.

--- components/hal-common/include/hal/storage_base.h
+++ components/hal-common/include/hal/storage_base.h
@@ -2,6 +2,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #include <hal/err.h>
 
--- components/hal-stm32/storage.c
+++ components/hal-stm32/storage.c
@@ -1,20 +1,82 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <wldb.h>
+
+#include <hal/log.h>
+#include <hal/rand.h>
 #include <hal/storage.h>
 
-// TODO: Whole file
+#if !defined(CONFIG_PAGE_SIZE)
+#error Missing CONFIG_PAGE_SIZE
+#endif
 
-void hal_storage_init(hal_storage_t *s, const char *name)
+#if !defined(CONFIG_BLOCK_SIZE)
+#error Missing CONFIG_BLOCK_SIZE
+#endif
+
+LOG_TAG_DECLARE("HAL.Storage");
+
+#define KEY_SIZE_MAX 8
+#define WLDB_RET_TO_HAL(ret) (ret < 0 ? ret : HAL_ERR_NONE)
+
+static wldb_t db;
+
+extern uint8_t __storage_start;
+extern uint8_t __storage_end;
+
+static size_t hal_storage_format_key(hal_storage_t *s, const void *key, size_t key_size, uint8_t *buf)
 {
+    if (key_size > KEY_SIZE_MAX - 1)
+    {
+        LOG_F(TAG, "Key size %u > %d", (unsigned)key_size, WLDB_KEY_SIZE_MAX);
+    }
+    buf[0] = s->ns;
+    memcpy(&buf[1], key, key_size);
+    return key_size + 1;
 }
 
-bool hal_storage_get_blob(hal_storage_t *s, const char *key, void *buf, size_t *size)
+hal_err_t hal_storage_init(hal_storage_t *s, uint8_t ns)
 {
-    return false;
+    static bool db_is_initialized = false;
+    if (!db_is_initialized)
+    {
+        int ret = wldb_init(&db, (wldb_addr_t)&__storage_start, (wldb_addr_t)&__storage_end, CONFIG_PAGE_SIZE, CONFIG_BLOCK_SIZE);
+        if (ret < 0)
+        {
+            return ret;
+        }
+        db_is_initialized = true;
+    }
+    s->ns = ns;
+    return HAL_ERR_NONE;
 }
 
-void hal_storage_set_blob(hal_storage_t *s, const char *key, const void *buf, size_t size)
+hal_err_t hal_storage_get_blob(hal_storage_t *s, const void *key, size_t key_size, void *buf, size_t *size, bool *found)
 {
+    uint8_t wkey[KEY_SIZE_MAX];
+    size_t ks = hal_storage_format_key(s, key, key_size, wkey);
+    int ret = wldb_get(&db, wkey, ks, buf, size);
+    if (found)
+    {
+        *found = ret > 0;
+    }
+    return WLDB_RET_TO_HAL(ret);
 }
 
-void hal_storage_commit(hal_storage_t *s)
+hal_err_t hal_storage_set_blob(hal_storage_t *s, const void *key, size_t key_size, const void *buf, size_t size)
 {
+    uint8_t wkey[KEY_SIZE_MAX];
+    size_t ks = hal_storage_format_key(s, key, key_size, wkey);
+    int ret = wldb_set(&db, wkey, ks, buf, size);
+    return WLDB_RET_TO_HAL(ret);
+}
+
+hal_err_t hal_storage_commit(hal_storage_t *s)
+{
+    (void)s;
+
+    int ret = wldb_commit(&db);
+    return WLDB_RET_TO_HAL(ret);
 }
